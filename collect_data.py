56beea04388e4278c65f3914c70bba14d63cea40
Max Laponsky <thebiglaponsky@gmail.com>
2016-07-24 13:34:13 -0400

    Initial commit with the start of daata collection

---
+++ collect_data.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+from urllib.parse import quote
+from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
+
+import pymysql as mysql
+import json
+import datetime
+import re
+import sys
+import time
+import os
+
+conn = mysql.connect(host='localhost', port=3306, username="root", pwd=os.environ.get('MYSQL_PWD'))
+cur = conn.cursor()
+
+BASE_URL = "http://www.ohhla.com/all.html"
+SECTION_LINKS = ['xx', 'a', 'b', 'c', 'd', 'e', 'all_two', 'all_three', 'all_four', 'all_five']
+BASE_DIV = "leftmain"
